fbd24ae07a7806595d130ccf7a0efa46c3bb7e4d
Soton Ahmed <soton371@gmail.com>
2024-10-21 21:43:58 +0600

    response organized

---
+++ app/custom_responses.py
@@ -0,0 +1,20 @@
+from fastapi.responses import JSONResponse
+
+def ResponseSuccess(status_code: int, data: any):
+    return JSONResponse(
+        status_code=status_code,
+        content={
+            "success": True,
+            "data": data
+        }
+    )
+
+def ResponseFailed(status_code: int, message: str):
+    return JSONResponse(
+        status_code=status_code,
+        content={
+            "success": False,
+            "message": message
+        }
+    )
+
--- app/routers/admin.py
+++ app/routers/admin.py
@@ -1,6 +1,5 @@
 from fastapi import status, Depends, APIRouter
-from fastapi.responses import JSONResponse
-
+from ..custom_responses import ResponseSuccess, ResponseFailed
 from .. import models, schemas, utils
 from ..database import get_db
 from sqlalchemy.orm import Session
@@ -10,16 +9,15 @@ router = APIRouter(
     tags=['Admin']
 )
 
-@router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.AdminOut)
+
+@router.post("/")
 async def create_admin(admin: schemas.AdminCreate, db: Session = Depends(get_db)):
     try:
-        existAdmin = db.query(models.Admin).filter(models.Admin.email == admin.email).first()
+        existAdmin = db.query(models.Admin).filter(
+            models.Admin.email == admin.email).first()
 
         if existAdmin:
-            return JSONResponse(
-                status_code=status.HTTP_208_ALREADY_REPORTED,
-                content={"success": False, "message": f"Admin with email {admin.email} already exists."}
-            )
+            return ResponseFailed(status_code=status.HTTP_208_ALREADY_REPORTED, message=f"Admin with email {admin.email} already exists.")
 
         hashedPassword = utils.hash(admin.password)
         admin.password = hashedPassword
@@ -27,25 +25,43 @@ async def create_admin(admin: schemas.AdminCreate, db: Session = Depends(get_db)
         db.add(new_admin)
         db.commit()
         db.refresh(new_admin)
-        
-        return new_admin
+        data = {
+            "id": new_admin.id,
+            "email": new_admin.email,
+            "created_at": str(new_admin.created_at)
+        }
+        return ResponseSuccess(status_code=status.HTTP_201_CREATED, data=data)
     except Exception as error:
-        return JSONResponse(
-                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                content={"success": False, "message": "Something went wrong!"}
-            )
+        print(f"error in create_admin: {error}")
+        return ResponseFailed(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            message="Something went wrong!"
+        )
+
 
 @router.get("/{id}")
 def get_admin(id: int, db: Session = Depends(get_db)):
-    user = db.query(models.Admin).filter(models.Admin.id == id).first()
-    
-    if not user:
-        return JSONResponse(
+    try:
+        adminFetch = db.query(models.Admin).filter(
+            models.Admin.id == id).first()
+
+        if not adminFetch:
+            return ResponseFailed(
                 status_code=status.HTTP_404_NOT_FOUND,
-                content={"success": False, "message": f"User with id {id} does not exist"}
+                message=f"Admin with id {id} does not exist"
             )
-    # return ResponseSuccess(data=user.id)
-    return JSONResponse(
-                status_code=status.HTTP_200_OK,
-                content={"success": True, "data": user.email}
-            )
+        data = {
+            "id": adminFetch.id,
+            "email": adminFetch.email,
+            "created_at": str(adminFetch.created_at)
+        }
+        return ResponseSuccess(
+            status_code=status.HTTP_200_OK,
+            data=data
+        )
+    except Exception as error:
+        print(f"error in get_admin: {error}")
+        return ResponseFailed(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            message="Something went wrong!"
+        )
